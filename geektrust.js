fedf704528f1a8e8c20535f1bf249c0d23e4859f
ayushman101 <amansinghayush@gmail.com>
2024-04-28 13:22:50 +0530

    template

---
+++ geektrust.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+
+const filename = process.argv[2]
+
+fs.readFile(filename, "utf8", (err, data) => {
+    if (err) throw err
+    
+    var inputLines = data.toString().split("\n") 
+    // var inputLines = data.toString().split("\r\n") 
+
+    console.log(inputLines)
+    
+})
